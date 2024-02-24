823a0f542133032e2eefffd4b9349242ba0714a9
AbdelrahmanAttef74 <abdoeldox@gmail.com>
2024-02-24 07:21:36 +0200

    create note list view

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:note_app/views/widgets/custom_app_bar.dart';
+import 'package:note_app/views/widgets/note_list_view.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
@@ -26,57 +26,9 @@ class NotesBody extends StatelessWidget {
             height: 40,
           ),
           CustomAppBar(),
-          NoteItem()
+          Expanded(child: NoteListView())
         ],
       ),
     );
-  }
-}
-
-class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          color: const Color(0xffffcd7b),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: const Text(
-                'Flutter Tips',
-                style: TextStyle(color: Colors.black, fontSize: 24),
-              ),
-              subtitle: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                child: Text(
-                  'Build your career with tharwat samy',
-                  style: TextStyle(
-                      color: Colors.black.withOpacity(.5), fontSize: 20),
-                ),
-              ),
-              trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  FontAwesomeIcons.trash,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 24),
-              child: Text(
-                'may21 ,2022',
-                style: TextStyle(
-                    color: Colors.black.withOpacity(.5), fontSize: 20),
-              ),
-            )
-          ],
-        ));
   }
 }
