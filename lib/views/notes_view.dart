977648d67e48503589cde20b210021d36a2ca290
AbdelrahmanAttef74 <abdoeldox@gmail.com>
2024-02-25 11:53:57 +0200

    add floating action button

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,34 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:note_app/views/widgets/custom_app_bar.dart';
-import 'package:note_app/views/widgets/note_list_view.dart';
+
+import 'widgets/notes_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: NotesBody(),
-    );
-  }
-}
-
-class NotesBody extends StatelessWidget {
-  const NotesBody({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 40,
-          ),
-          CustomAppBar(),
-          Expanded(child: NoteListView())
-        ],
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
+      body: const NotesBody(),
     );
   }
 }
---
+++ lib/views/widgets/notes_body.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/views/widgets/custom_app_bar.dart';
+import 'package:note_app/views/widgets/note_list_view.dart';
+
+class NotesBody extends StatelessWidget {
+  const NotesBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 40,
+          ),
+          CustomAppBar(),
+          Expanded(child: NoteListView())
+        ],
+      ),
+    );
+  }
+}
