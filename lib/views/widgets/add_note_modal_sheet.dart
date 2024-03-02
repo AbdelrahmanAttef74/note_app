37215757e4bf33a89e81013191d282d83df6fc74
AbdelrahmanAttef74 <abdoeldox@gmail.com>
2024-03-02 23:26:54 +0200

    refresh notes list

--- lib/views/widgets/add_note_modal_sheet.dart
+++ lib/views/widgets/add_note_modal_sheet.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:note_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:note_app/views/widgets/add_note_form.dart';
 
 class AddNote extends StatelessWidget {
@@ -12,10 +13,9 @@ class AddNote extends StatelessWidget {
       create: (context) => AddNoteCubit(),
       child: BlocConsumer<AddNoteCubit, AddNoteState>(
         listener: (context, state) {
-          if (state is AddNoteFailer) {
-            print('Failed ${state.errorMessage}');
-          }
+          if (state is AddNoteFailer) {}
           if (state is AddNoteSuccess) {
+            BlocProvider.of<NotesCubit>(context).fetchAllNotes();
             Navigator.pop(context);
           }
         },
